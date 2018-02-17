194c50fdf4574005a2cc0d46c8a4c739d7e753b9
Kenji Hirabayashi <mkhira2@gmail.com>
2018-02-17 15:30:32 -0600

    adds cancel and continue buttons

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,6 +75,10 @@ purchaseCancelHandler = () => {
     this.setState({purchasing: false});
 }
 
+purchaseContinueHandler = () => {
+    alert('You continue!');
+}
+
     render () {
         const disabledInfo = {
             ...this.state.ingredients
@@ -85,7 +89,9 @@ purchaseCancelHandler = () => {
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary ingredients={this.state.ingredients}
+                    purchaseCancelled={this.purchaseCancelHandler}
+                    purchaseContinued={this.purchaseContinueHandler} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
