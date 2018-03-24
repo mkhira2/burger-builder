4f5c80c11172e5ac78ceda4799a71af23e6e2678
Kenji Hirabayashi <mkhira2@gmail.com>
2018-03-24 16:35:19 -0500

    sends post requests to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -76,7 +77,23 @@ purchaseCancelHandler = () => {
 }
 
 purchaseContinueHandler = () => {
-    alert('You continue!');
+    const order = {
+        ingredients: this.state.ingredients,
+        price: this.state.totalPrice,
+        customer: {
+            name: 'Kenji Hirabayashi',
+            address: {
+                street: '125 Burger Drive',
+                zipCode: '90210',
+                country: 'United States'
+            },
+            email: 'ilove@burgers.com',
+        },
+        deliveryMethod: 'fastest'
+    }
+    axios.post('/orders.json', order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
 }
 
     render () {
