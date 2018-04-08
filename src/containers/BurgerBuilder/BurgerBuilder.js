d66719a72608d1ac03526b25d3422af3bd35da71
Kenji Hirabayashi <mkhira2@gmail.com>
2018-04-08 15:13:43 -0500

    order submission and passing data between pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -86,32 +86,11 @@ purchaseCancelHandler = () => {
 }
 
 purchaseContinueHandler = () => {
-    // this.setState({loading: true})
-    // const order = {
-    //     ingredients: this.state.ingredients,
-    //     price: this.state.totalPrice,
-    //     customer: {
-    //         name: 'Kenji Hirabayashi',
-    //         address: {
-    //             street: '125 Burger Drive',
-    //             zipCode: '90210',
-    //             country: 'United States'
-    //         },
-    //         email: 'ilove@burgers.com',
-    //     },
-    //     deliveryMethod: 'fastest'
-    // }
-    // axios.post('/orders.json', order)
-    //     .then(response => {
-    //         this.setState({ loading: false, purchasing: false })
-    //     })
-    //     .catch(error => {
-    //         this.setState({ loading: false, purchasing: false })
-        // });
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
